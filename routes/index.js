5d0703fb8825747f9790e31909e203941a5cfa03
Tongda <tongda.zhang@gmail.com>
2013-10-19 00:39:23 -0700

    chaneg default page

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'CHIME LAB SIMULATION' , link: req.param('type')});
 };
+
+exports.welcomePage=function(req,res){
+	res.render('welcomePage',{title: 'CHIME LAB SIMULATION'});
+}
